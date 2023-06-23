60968e10aa6348d916789d6e040300f5302bba06
darth-maul <patrick.kings@live.com>
2023-06-23 19:10:31 +0300

    initial

---
+++ .wezterm.lua
@@ -0,0 +1,19 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font_size = 10.0
+
+-- lauch program on start
+-- config.default_prog = {'tmux'}
+
+-- and finally, return the configuration to wezterm
+return config
